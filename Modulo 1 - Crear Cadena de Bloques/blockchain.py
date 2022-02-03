e3ed7bff2fbd0c023f6f63d32a9c5c15d451ef5f
lucianopulido <lucianopulido1996@gmail.com>
2022-02-03 17:56:37 -0300

    se creo constructor de la cadena de bloques

--- Modulo 1 - Crear Cadena de Bloques/blockchain.py
+++ Modulo 1 - Crear Cadena de Bloques/blockchain.py
@@ -4,5 +4,22 @@ Created on Wed Feb  2 21:59:54 2022
 
 @author: Luciano
 """
-#Modulo 1 - Crear una Cadena de Bloques
+# Modulo 1 - Crear una Cadena de Bloques
 
+# Importar las librerias
+import datetime
+import hashlib
+import json
+from flask import  Flask, jsonify
+
+# Parte 1 - Crear la cadena de bloques
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0' )
+        
+
+
+# Parte 2 - Minado de un bloque de una cadena
