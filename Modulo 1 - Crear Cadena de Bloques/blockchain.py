441bfed3a51d13265c077cb86b5fbda5c7328145
lucianopulido <lucianopulido1996@gmail.com>
2022-02-10 18:24:39 -0300

    se creo método para crear bloques de la cadena de bloques

--- Modulo 1 - Crear Cadena de Bloques/blockchain.py
+++ Modulo 1 - Crear Cadena de Bloques/blockchain.py
@@ -10,16 +10,24 @@ Created on Wed Feb  2 21:59:54 2022
 import datetime
 import hashlib
 import json
-from flask import  Flask, jsonify
+from flask import Flask, jsonify
+
 
 # Parte 1 - Crear la cadena de bloques
 
 class Blockchain:
-    
+
     def __init__(self):
         self.chain = []
-        self.create_block(proof = 1, previous_hash = '0' )
-        
+        self.create_block(proof=1, previous_hash='0')
 
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash
+                 }
+        self.chain.append(block)
+        return block
 
 # Parte 2 - Minado de un bloque de una cadena
