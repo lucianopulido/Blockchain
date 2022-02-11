3aac64fdbd91ebb86f203406bfe9b498b806cff8
lucianopulido <lucianopulido1996@gmail.com>
2022-02-11 13:05:52 -0300

    se agrego el método para minar un bloque

--- Modulo 1 - Crear Cadena de Bloques/blockchain.py
+++ Modulo 1 - Crear Cadena de Bloques/blockchain.py
@@ -28,11 +28,29 @@ class Blockchain:
                  'proof': proof,
                  'previous_hash': previous_hash
                  }
+
         self.chain.append(block)
         return block
 
     # Este método sirve para obtener el último bloque
     def get_previous_block(self):
         return self.chain[-1]
+
+    # Este método sirve para minar un bloque
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+
+        while check_proof is False:
+            # esta linea obtiene el hash, al cual tenemos que verificar que tenga tantos ceros a la izquierda y que sea menor
+            # a un hash dado. Se crea el problema a resolver por el minero con una ecuación como parametro de la función
+            # hash 256, se lo convierte a string, le da el formato correcto de sha256 y se lo vuelve a convertir a un número de 64 bytes
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[0:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+
+        return new_proof
 
 # Parte 2 - Minado de un bloque de una cadena
